55c7805f5980873874976fff228aa027536a1977
BgeFlexTeam <BGEFLEX@gmail.com>
2017-08-23 14:22:47 +0200

    Add Kitting station 1.0 (not complete yet)

---
+++ FlexPTLBGEWeb/Controllers/KittingController.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Mvc;
+using FlexPTLBGEWeb.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System;
+using System.Linq;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+
+namespace FlexPTLBGEWeb.Controllers
+{
+    public class KittingController : Controller
+    {
+        public IDbConnection Connection { get; }
+        private readonly AppOptions options;
+        private string connectionString;
+
+        public KittingController(IOptions<AppOptions> options)
+        {
+            // Connection = new SqlConnection("data source=ZALNT254;initial catalog=PTLBGE;persist security info=True;user id=web;password=connect!;App=FlexPTLBGEWeb");
+            //Connection.Open();
+            this.options = options.Value;
+            connectionString = this.options.DefaultConnection;
+        }      
+
+        [HttpPost]      
+        public List<Part> GetBoms([FromBody] Product p)
+        {
+            try{
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    DynamicParameters parameters = new DynamicParameters();       
+                    parameters.Add("@SerialNumber", p.SerialNumber);
+                    List<Part> result = connection.Query<Part>("SELECT p.* FROM Product pr INNER JOIN BOM b ON pr.PartID = b.ParentID INNER JOIN Part p ON p.ID = b.PartID WHERE pr.SerialNumber = @SerialNumber order by b.AssemblyOrder", parameters).ToList();
+                    return result;
+                }
+            }catch(Exception e){
+              System.Console.WriteLine(e);
+                return new List<Part>();
+
+            }
+        }
+        
+        [HttpPost]      
+        public bool InsertToPTL([FromBody] Part p)
+        { 
+            //-- Test:		EXEC ptlAddRequest 'FlexFlow', '<ptl><application><name>FlexFlow</name><version>2.9.3.100</version></application><signal>PICK</signal><signalref>N/A</signalref><request><rpos>1</rpos><line>X86-ASSY01</line><zone>X86A01</zone><uniqid>S40017W</uniqid><description>SYS100782</description><data><item>LGH-0000000FW856</item><qty>4</qty></data><data><item>LGH-0000000KG396</item><qty>2</qty></data></request></ptl>'
+            try{
+                string sql = "dbo.ptlAddRequest";
+                string xml_param="<ptl><application><name>PTLBGE</name><version>1.0</version></application><signal>PICK</signal><signalref>N/A</signalref>";
+                xml_param+="<request><rpos>1</rpos><line>Line1</line><zone>Zone1</zone><uniqid>1</uniqid><description>-</description>";
+                xml_param+="<data><item>LGH-0000000FW856</item><qty>4</qty></data></request></ptl>";
+                var param = new DynamicParameters();
+                param.Add("@Request", xml_param);
+
+               
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    var affectedRows = connection.Execute(sql, param, commandType: CommandType.StoredProcedure);
+                    return affectedRows>0;                    
+                }
+            }catch(Exception e){
+                System.Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+    }
+}
--- FlexPTLBGEWeb/wwwroot/src/kitting-station.ts
+++ FlexPTLBGEWeb/wwwroot/src/kitting-station.ts
@@ -3,4 +3,57 @@
 
 @customElement("kitting-station")
 class KittingStation extends Polymer.Element {
+
+    @property()
+    prod: Project.Product = null;
+
+    connectedCallback(): void {
+        super.connectedCallback();
+        this.$.kittingSnInputBox.value = "";
+        this.$.kittinglabel.innerText = "";
+        this.$.kittingSnInputBox.focus();
+        this.$.kittingSnInputBox.focused = true;
+    }
+
+    async _txtKittingKeypress(e: any): Promise<void> {
+        if (e.key === "Enter") { this.kittingSNTap(); }
+    }
+
+    async kittingSNTap(): Promise<any> {
+        this.$.kittinglabel.innerText = "loading...";
+        let p: Project.Product = new Project.Product();
+        p.serialNumber = this.$.kittingSnInputBox.value;
+        if (p.serialNumber != null && p.serialNumber.length > 0) {
+            this.prod = await Project.Fetch.Post(`/api/Product/getProductBySN`, p);
+        }
+        if (this.prod != null) {
+            let b: boolean = await this.kittingSN();
+            if (b) {
+                this.$.kittinglabel.innerText = "BOM list loaded.";
+                this.$.kittingSnInputBox.focus();
+                this.$.kittingSnInputBox.focused = true;
+            } else {
+                this.$.kittingSnInputBox.value = "";
+                this.$.kittinglabel.innerText = "Something went wrong...";
+                this.$.kittingSnInputBox.focus();
+                this.$.kittingSnInputBox.focused = true;
+            }
+        }
+
+    }
+
+    async kittingSN(): Promise<boolean> {
+        try {
+            let list: Array<Project.Part> = await Project.Fetch.Post(`/api/Kitting/GetBoms`, this.prod);
+            if (list.length === 0) { return false; }
+            this.$.bomlist.items = list;
+            // insert to PTL
+            for (let v of list) {
+                await Project.Fetch.Post(`/api/Kitting/InsertToPTL`, v);
+            }
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
 }

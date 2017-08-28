70aa395baa2c3635045a186527ec7655bf48fdc6
zalkkoll <krisztian.koller@flex.com>
2017-08-28 13:37:22 +0200

    Publish.

--- FlexPTLBGEWeb/publish.bat
+++ FlexPTLBGEWeb/publish.bat
@@ -1,5 +1,8 @@
+rem gulp ts
+rem gulp ob
 dotnet publish -c release -r win10-x64
 Obfuscar.Console.exe Obfuscar.Config.xml
+del bin\release\netcoreapp2.0\win10-x64\publish\wwwroot\src\* /q
 copy bin\release\netcoreapp2.0\win10-x64\publish\obfuscar\FlexPTLBGEWeb.dll bin\release\netcoreapp2.0\win10-x64\publish /y
 del bin\release\netcoreapp2.0\win10-x64\publish\obfuscar\* /q
 rd bin\release\netcoreapp2.0\win10-x64\publish\obfuscar /q
